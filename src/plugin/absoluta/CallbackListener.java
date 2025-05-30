2cc1e5471f5a822194909e1043598d4de6d4a09b
Banshika <stefa.zardoni@me.com>
2025-05-30 09:15:59 +0200

    Correzioni indent

--- src/plugin/absoluta/CallbackListener.java
+++ src/plugin/absoluta/CallbackListener.java
@@ -30,67 +30,67 @@ class CallbackListener implements PropertyChangeListener {
             String indexProperyString = String.valueOf(indexProperty);
             String propertyName = property.getPropertyName();
             switch(propertyName) {
-            case "PARTITION_ARMING":
-               this.callback.setPartitionArming(indexProperyString, this.panelStatus.getPartitionArming(indexProperty));
-               break;
-            case "PARTITION_STATUS":
-               this.callback.setPartitionStatus(indexProperyString, this.panelStatus.getPartitionStatus(indexProperty));
-               break;
-            case "PARTITION_LABEL":
-               this.callback.setPartitionRemoteName(indexProperyString, this.panelStatus.getPartitionLabel(indexProperty));
-               break;
-            case "ZONE_STATUS":
-               this.callback.setInputStatus(indexProperyString, this.panelStatus.getZoneStatus(indexProperty));
-               break;
-            case "ZONE_LABEL":
-               this.callback.setInputRemoteName(indexProperyString, this.panelStatus.getZoneLabel(indexProperty));
-               break;
-            case "OUTPUT_STATUS":
-               this.callback.setOutputStatus(indexProperyString, this.panelStatus.getOutputStatus(indexProperty));
-               break;
-            case "OUTPUT_LABEL":
-               this.callback.setOutputRemoteName(indexProperyString, this.panelStatus.getOutputLabel(indexProperty));
-               break;
-            case "ARMING_MODE_LABEL":
-               Character charMode = (Character)CustomizedArmingModes.ARMING_MODE_LABELS.get(indexProperty);
-               if (charMode != null) {
-                  this.callback.setLabelArming(charMode, this.panelStatus.getArmingModeLabel(indexProperty));
-               }
-               break;
+               case "PARTITION_ARMING":
+                  this.callback.setPartitionArming(indexProperyString, this.panelStatus.getPartitionArming(indexProperty));
+                  break;
+               case "PARTITION_STATUS":
+                  this.callback.setPartitionStatus(indexProperyString, this.panelStatus.getPartitionStatus(indexProperty));
+                  break;
+               case "PARTITION_LABEL":
+                  this.callback.setPartitionRemoteName(indexProperyString, this.panelStatus.getPartitionLabel(indexProperty));
+                  break;
+               case "ZONE_STATUS":
+                  this.callback.setInputStatus(indexProperyString, this.panelStatus.getZoneStatus(indexProperty));
+                  break;
+               case "ZONE_LABEL":
+                  this.callback.setInputRemoteName(indexProperyString, this.panelStatus.getZoneLabel(indexProperty));
+                  break;
+               case "OUTPUT_STATUS":
+                  this.callback.setOutputStatus(indexProperyString, this.panelStatus.getOutputStatus(indexProperty));
+                  break;
+               case "OUTPUT_LABEL":
+                  this.callback.setOutputRemoteName(indexProperyString, this.panelStatus.getOutputLabel(indexProperty));
+                  break;
+               case "ARMING_MODE_LABEL":
+                  Character charMode = (Character)CustomizedArmingModes.ARMING_MODE_LABELS.get(indexProperty);
+                  if (charMode != null) {
+                     this.callback.setLabelArming(charMode, this.panelStatus.getArmingModeLabel(indexProperty));
+                  }
+                  break;
             }
          }
          else {
             String propertyName = property.getPropertyName();
             switch(propertyName) {
-            case "CONNECTION_STATUS":
-               if (PanelStatus.ConnectionStatus.CONNECTED == property.getOldValue() && PanelStatus.ConnectionStatus.DISCONNECTED == property.getNewValue()) {
-                  this.callback.connectionLost();
-               }
-               break;
-            case "GLOBAL_ARMING":
-               this.callback.setArming(this.panelStatus.getGlobalArming());
-               break;
-            case "SYSTEM_LABEL":
-               break;
-            case "PARTITIONS":
-               this.callback.changePartitions(toStringList(this.panelStatus.getPartitions()));
-               break;
-            case "ZONES":
-               List<String> var10 = toStringList(this.panelStatus.getZones());
-               this.callback.changeInputs(var10);
-               List<String> var13 = toStringList(this.panelStatus.getPartitions());
-               Iterator var12 = var13.iterator();
+               case "CONNECTION_STATUS":
+                  if (PanelStatus.ConnectionStatus.CONNECTED == property.getOldValue() && PanelStatus.ConnectionStatus.DISCONNECTED == property.getNewValue()) {
+                     this.callback.connectionLost();
+                  }
+                  break;
+               case "GLOBAL_ARMING":
+                  this.callback.setArming(this.panelStatus.getGlobalArming());
+                  break;
+               case "SYSTEM_LABEL":
+                  break;
+               case "PARTITIONS":
+                  this.callback.changePartitions(toStringList(this.panelStatus.getPartitions()));
+                  break;
+               case "ZONES":
+                  List<String> var10 = toStringList(this.panelStatus.getZones());
+                  this.callback.changeInputs(var10);
+                  List<String> var13 = toStringList(this.panelStatus.getPartitions());
+                  Iterator var12 = var13.iterator();
 
-               while(var12.hasNext()) {
-                  String var7 = (String)var12.next();
-                  this.callback.tagInputIntoPartition(var7, var10);
-               }
-               return;
-            case "OUTPUTS":
-               this.callback.changeOutputs(toStringList(this.panelStatus.getOutputs()));
-               break;
-            case "TROUBLES":
-               break;
+                  while(var12.hasNext()) {
+                     String var7 = (String)var12.next();
+                     this.callback.tagInputIntoPartition(var7, var10);
+                  }
+                  return;
+               case "OUTPUTS":
+                  this.callback.changeOutputs(toStringList(this.panelStatus.getOutputs()));
+                  break;
+               case "TROUBLES":
+                  break;
             }
          }
       }
