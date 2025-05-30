9215165e1ca086e8fec07e3b28d5b034dd9390fa
Banshika <stefa.zardoni@me.com>
2025-05-30 08:21:24 +0200

    Rimozione elementi non utilizzati

--- src/cms/device/api/DeviceType.java
+++
@@ -1,9 +0,0 @@
-package cms.device.api;
-
-public final class DeviceType implements Comparable<DeviceType> {
-
-   public int compareTo(DeviceType var1) {
-      return this.toString().compareToIgnoreCase(var1.toString());
-   }
-
-}
