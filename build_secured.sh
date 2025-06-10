9e5adda965f35f844da01626fd6d2f3d2e605b90
Shikaban <92381214+Shikaban@users.noreply.github.com>
2025-06-10 20:44:59 +0200

    Cambiato ordine compilazione jar

--- build_secured.sh
+++ build_secured.sh
@@ -4,22 +4,22 @@
 # 1. Crea la cartella build se non esiste
 mkdir -p build
 
-# 2. Compila e crea cms.jar
+# 2. Compila e crea protocol.jar
+find src/protocol -name "*.java" > protocol_sources.txt
+javac --release 21 -cp "lib/jars/*:secured/*:build" -d build @protocol_sources.txt
+jar cf secured/protocol.jar -C build/protocol .
+rm protocol_sources.txt
+
+# 3. Compila e crea cms.jar
 find src/cms -name "*.java" > cms_sources.txt
 javac --release 21 -cp "lib/jars/*:secured/*" -d build @cms_sources.txt
 jar cf secured/cms.jar -C build/cms .
 rm cms_sources.txt
 
-# 3. Compila e crea plugin.jar
+# 4. Compila e crea plugin.jar
 find src/plugin -name "*.java" > plugin_sources.txt
 javac --release 21 -cp "lib/jars/*:secured/*:build" -d build @plugin_sources.txt
 jar cf secured/plugin.jar -C build/plugin .
 rm plugin_sources.txt
-
-# 4. Compila e crea protocol.jar
-find src/protocol -name "*.java" > protocol_sources.txt
-javac --release 21 -cp "lib/jars/*:secured/*:build" -d build @protocol_sources.txt
-jar cf secured/protocol.jar -C build/protocol .
-rm protocol_sources.txt
 
 echo "Compilazione e creazione dei JAR in secured/ completata."
